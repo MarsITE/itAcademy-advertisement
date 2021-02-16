bfe1a090233e76c4e25a87afa1b7b2f16fd3f072
Andrii Prybyla <mars06081985@gmail.com>
2021-02-16 17:59:30 +0200

    Add homeServlet

--- src/main/java/academy/softserve/controller/HomeServlet.java
+++ src/main/java/academy/softserve/controller/HomeServlet.java
@@ -1,9 +1,13 @@
 package academy.softserve.controller;
 
 import academy.softserve.model.Advert;
+import academy.softserve.model.User;
 import academy.softserve.model.library.AdvertGenre;
+import academy.softserve.model.library.UserRole;
+import academy.softserve.model.library.UserStatus;
 import academy.softserve.service.AdvertService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,38 +26,96 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String advertId = request.getParameter("advertId");
-        if (advertId != null){
-            advertService.update(setAdvert(request));
-        } else {
-            advertService.save(setAdvert(request));
-        }
-        request.setAttribute("genres", AdvertGenre.values());
-        request.setAttribute("adverts", advertService.findAll());
-        request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+        doGet(request, response);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String advertId = request.getParameter("advertId");
-        request.setAttribute("genres", AdvertGenre.values());
-        request.setAttribute("adverts", advertService.findAll());
-        request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+        String action = request.getServletPath();
+        switch (action) {
+            case "/new":
+                showNewForm(request, response);
+                break;
+            case "/insert":
+                insertAdvert(request, response);
+                break;
+            case "/delete":
+                deleteAdvert(request, response);
+                break;
+            case "/edit":
+                showEditForm(request, response);
+                break;
+            case "/update":
+                updateAdvert(request, response);
+                break;
+            default:
+                listAdvert(request, response);
+                break;
+        }
     }
 
+    private void listAdvert(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        adverts = advertService.findAll();
+        request.setAttribute("adverts", adverts);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/advert-list.jsp");
+        dispatcher.forward(request, response);
+    }
 
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/advert-form.jsp");
+        dispatcher.forward(request, response);
+    }
 
-    private Advert setAdvert(HttpServletRequest request){
-       advert = new Advert();
-        return advert.builder()
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        long id = Long.parseLong(request.getParameter("advertId"));
+        Advert existingAdvert = advertService.findById(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/advert-form.jsp");
+        request.setAttribute("advert", existingAdvert);
+        dispatcher.forward(request, response);
+
+    }
+
+    private void insertAdvert(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        advert = advert.builder()
                 .title(request.getParameter("title"))
                 .description(request.getParameter("description"))
                 .publishingDate(LocalDate.parse(request.getParameter("publishingDate")))
                 .endingDate(LocalDate.parse(request.getParameter("endingDate")))
                 .advertGenre(AdvertGenre.valueOf(request.getParameter("advertGenre")))
                 .build();
+        advertService.save(advert);
+        response.sendRedirect("list");
     }
 
+    private void updateAdvert(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        long advertId = Integer.parseInt(request.getParameter("advertId"));
+        User user1 = User.builder().id(1).firstName("Andrii").lastName("Prybyla").password("1234")
+                .dateOfBirth(LocalDate.of(1985, 8, 6)).email("mars@ukr.net")
+                .userRole(UserRole.USER).userStatus(UserStatus.NEWCOMER).build();
+        advert = advert.builder()
+                .title(request.getParameter("title"))
+                .description(request.getParameter("description"))
+                .publishingDate(LocalDate.parse(request.getParameter("publishingDate")))
+                .endingDate(LocalDate.parse(request.getParameter("endingDate")))
+                .advertGenre(AdvertGenre.valueOf(request.getParameter("advertGenre")))
+                .author(user1)
+                .build();
 
+        advertService.update(advert);
+        response.sendRedirect("list");
+    }
+
+    private void deleteAdvert(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        long advertId = Long.parseLong(request.getParameter("advertId"));
+        advertService.delete(advertId);
+        response.sendRedirect("list");
+
+    }
 
 }
---
+++ src/main/webapp/WEB-INF/pages/advert-form.jsp
@@ -0,0 +1,91 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>Adverts Form</title>
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark"
+         style="background-color: tomato">
+
+        <ul class="navbar-nav">
+            <li><a href="<%=request.getContextPath()%>/list"
+                   class="nav-link">Adverts</a></li>
+        </ul>
+    </nav>
+</header>
+<br>
+<div class="container col-md-5">
+    <div class="card">
+        <div class="card-body">
+            <c:if test="${advert != null}">
+            <form action="update" method="post">
+                </c:if>
+                <c:if test="${advert == null}">
+                <form action="insert" method="post">
+                    </c:if>
+
+                    <caption>
+                        <h2>
+                            <c:if test="${advert != null}">
+                                Edit Advert
+                            </c:if>
+                            <c:if test="${advert == null}">
+                                Add New Advert
+                            </c:if>
+                        </h2>
+                    </caption>
+
+                    <c:if test="${advert != null}">
+                        <input type="hidden" name="advertId" value="<c:out value='${advert.id}' />"/>
+                    </c:if>
+
+                    <fieldset class="form-group">
+                        <label>Advert Title</label> <input type="text"
+                                                           value="<c:out value='${advert.title}' />"
+                                                           class="form-control"
+                                                           name="title" required="required">
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <label>Advert Description</label> <input type="text"
+                                                                 value="<c:out value='${advert.description}' />"
+                                                                 class="form-control"
+                                                                 name="description" required="required">
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <label>Advert Publishing Date</label> <input type="text"
+                                                                     value="<c:out value='${advert.publishingDate}' />"
+                                                                     class="form-control"
+                                                                     name="publishingDate" required="required">
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <label>Advert Ending Date</label> <input type="text"
+                                                                 value="<c:out value='${advert.endingDate}' />"
+                                                                 class="form-control"
+                                                                 name="endingDate" required="required">
+                    </fieldset>
+
+                    <fieldset class="form-group">
+                        <label>Advert Genre</label> <input type="text"
+                                                           value="<c:out value='${advert.advertGenre.name}' />"
+                                                           class="form-control"
+                                                           name="advertGenre" required="required">
+                    </fieldset>
+
+                    <button type="submit" class="btn btn-success">Save</button>
+                </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
